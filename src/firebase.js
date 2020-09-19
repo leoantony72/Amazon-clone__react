2e422f6ec048f7a32042b352625f57fe685bfd47
leoantony <leoantony72@gmail.com>
2020-09-20 00:52:58 +0530

    Amazon-clone/Home-Page/

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBd3KUPsjD7yPoAV_QJ-N1OQZKazj6fQSk",
+    authDomain: "challenge-37308.firebaseapp.com",
+    databaseURL: "https://challenge-37308.firebaseio.com",
+    projectId: "challenge-37308",
+    storageBucket: "challenge-37308.appspot.com",
+    messagingSenderId: "980379916722",
+    appId: "1:980379916722:web:1a5fb6a34fb2e1dc6decab",
+    measurementId: "G-J5RKCPB8PP"
+  };
